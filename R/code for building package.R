ad6c0647f1342405bf415c934f36e8411a55e147
William Claster <william108@gmail.com>
2022-01-23 17:44:45 +0900

    uploading package with Chapter 16 as well.

---
+++ R/code for building package.R
@@ -0,0 +1,11 @@
+library(devtools)
+library(usethis)
+library(roxygen2)
+library(learnr)
+
+usethis::create_package("C:/Users/willi/Documents/R4DSTutorial2nd")
+
+usethis::use_dev_package("gradethis")
+usethis::use_dev_package("tidyverse")
+
+usethis::use_dev_package("nycflights13")
---
+++ R/more code for creating package.R
@@ -0,0 +1,1 @@
+usethis::use_tutorial("tut5and6and16", "Chapters 3,5,16", open = interactive())
